8aa84b9d33fe88cb57281e1bf18315cc3dbbb117
Kurtis Jungersen <kmjungersen@gmail.com>
2014-06-11 12:34:37 -0400

    Fixed the render module to return a 'RenderResult' object instead of just an html string, and then added documentation

--- mfr_rst/render.py
+++ mfr_rst/render.py
@@ -2,6 +2,19 @@
 
 from docutils.core import publish_parts
 
+from mfr import RenderResult
+
 def render_html(fp, *args, **kwargs):
-    htmlstring = publish_parts(fp.read(), writer_name='html')['html_body']
-    return htmlstring
+    '''This function takes a file, reads it, and then passes
+    the content to Docutils for rendering.  After rendering
+    the html content, the function returns a RenderResult
+    object.
+
+    Note: In this case, we only render the html_body because
+    this html will be nested in an existing webpage.
+    '''
+    file_content = fp.read()
+    html_string = publish_parts(
+        file_content, writer_name='html')['html_body']
+
+    return RenderResult(html_string)
